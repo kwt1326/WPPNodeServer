ba44edc3288f18b49b6ebd11cc29a4b9cfc0c1c3
kwt1326 <u1326@hotmail.com>
2019-10-29 12:37:48 +0900

    20191029 [redis test]

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ if(process.env.NODE_ENV === "production")
 {
     const redis = require('redis');
     const RedisStore = require('connect-redis')(session);
-    const cloud = redis.createClient("redis://" + process.env.REDISCLOUD_URL, {no_ready_check : true});
+    const cloud = redis.createClient(process.env.REDISCLOUD_URL, {no_ready_check : true});
 
     sessionoption['store'] = new RedisStore({ client: cloud });
 }
