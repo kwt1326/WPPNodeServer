071ee68c7e63a631cb0855052b5fc25531416e46
kwt1326 <u1326@hotmail.com>
2019-10-30 18:12:24 +0900

    20191030 [redis test]

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ sequelize.sync();
 
 // CORS 
 app.all('/*', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'https://www.aquaclub.club');
     res.header('Access-Control-Allow-Methods', 'POST, PUT, GET, PATCH, UPDATE, DELETE');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
     res.header('Access-Control-Allow-Credentials', true);
