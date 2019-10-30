59bac216a8c8751b1afb0769d91c31075a2bf643
kwt1326 <u1326@hotmail.com>
2019-10-30 18:03:49 +0900

    20191030 [redis test]

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ sequelize.sync();
 // CORS 
 app.all('/*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Methods', 'POST, PUT, GET, DELETE');
+    res.header('Access-Control-Allow-Methods', 'POST, PUT, GET, PATCH, UPDATE, DELETE');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
     res.header('Access-Control-Allow-Credentials', true);
     next();
