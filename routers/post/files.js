6c1c041f2282e8bf6e2b3ac50e0f9ddc2cffbe93
kwt1326 <u1326@hotmail.com>
2019-11-01 17:56:39 +0900

    20191101 [cloudinary test]

--- routers/post/files.js
+++ routers/post/files.js
@@ -39,12 +39,11 @@ router.post('/', verifyToken, uploadSetting.single('img'), (req, res) => {
 // production - cloudinary uploader
 router.post('/ci', verifyToken, uploadSetting.single('img'), (req,res) => {
     const file = req.file;
-    console.log(req);
 
     const ext = path.extname(file.originalname);
     const newName = path.basename(file.originalname, ext) + Date.now() + ext;
 
-    cloudinary._upload_file.upload(file.originalname, function(result) { 
+    cloudinary.uploader.upload(file.originalname, function(result) { 
         console.log(result) 
         res.send({url : newName});
     }, {public_id: newName})
